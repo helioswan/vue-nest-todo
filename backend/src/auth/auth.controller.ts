e78d4016147ca2f63b9e266ab50e0085c82fc7d5
helioswan <helios.wan@hotmail.fr>
2025-04-03 10:57:21 +0200

    Add login, signup, refresh token routes

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignupDto } from './dtos/signup.dto';
+import { LoginDto } from './dtos/login.dto';
+import { RefreshTokenDto } from './dtos/refreshToken.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async signup(@Body() signupDto: SignupDto) {}
+
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {}
+
+  @Post('refreshToken')
+  refresh(@Body() refreshTokenDto: RefreshTokenDto) {}
+}
