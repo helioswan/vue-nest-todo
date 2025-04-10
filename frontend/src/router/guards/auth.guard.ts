84602a9ae656416bd69dce01384333200aba1970
helioswan <helios.wan@hotmail.fr>
2025-04-10 17:44:52 +0200

    Add auth guard

---
+++ frontend/src/router/guards/auth.guard.ts
@@ -0,0 +1,10 @@
+import { useAuthStore } from '@/stores/auth.store'
+import type { RouteLocationNormalized, RouteLocationNormalizedLoaded } from 'vue-router'
+
+export async function authGuard(to: RouteLocationNormalized, from: RouteLocationNormalizedLoaded) {
+  const { isAuthenticated } = useAuthStore()
+
+  if (!isAuthenticated && to.name !== 'Login') {
+    return { name: 'Login' }
+  }
+}
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { authGuard } from './guards/auth.guard'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +8,18 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
+      beforeEnter: authGuard,
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/signin',
+      name: 'signin',
+      component: () => import('../views/SigninView.vue'),
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: () => import('../views/SignupView.vue'),
     },
   ],
 })
