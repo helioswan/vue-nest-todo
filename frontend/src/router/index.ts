0e14ab6dff20dc8f665cb11377b6abcf92885401
helioswan <helios.wan@hotmail.fr>
2025-04-10 22:23:22 +0200

    Remove lazy loading for auth views

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { authGuard } from './guards/auth.guard'
+import SigninView from '../views/SigninView.vue'
+import SignupView from '../views/SignupView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,12 +15,12 @@ const router = createRouter({
     {
       path: '/signin',
       name: 'signin',
-      component: () => import('../views/SigninView.vue'),
+      component: SigninView,
     },
     {
       path: '/signup',
       name: 'signup',
-      component: () => import('../views/SignupView.vue'),
+      component: SignupView,
     },
   ],
 })
