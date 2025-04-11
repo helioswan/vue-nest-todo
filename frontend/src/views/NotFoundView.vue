eb8cb67b8c9b7137fe213896e4b3917c73ea0c8f
helioswan <helios.wan@hotmail.fr>
2025-04-11 23:57:58 +0200

    Add not found view

---
+++ frontend/src/views/NotFoundView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex items-center flex-col gap-6 justify-center mt-16">
+    <h1 class="text-2xl font-bold mb-2">Page not found</h1>
+    <UButton label="Go to home page" :to="{ name: 'home' }"></UButton>
+  </div>
+</template>
