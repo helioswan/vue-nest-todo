6c9d7d4c01ce4722172efb3bfbd2d1c667eb54c8
helioswan <helios.wan@hotmail.fr>
2025-04-14 13:51:47 +0200

    Restyle header

--- frontend/src/components/layout/Header.vue
+++ frontend/src/components/layout/Header.vue
@@ -1,14 +1,12 @@
+<script setup lang="ts"></script>
+
 <template>
-  <header
-    class="sticky top-0 left-0 w-full border-b py-4 md:px-6 px-5 dark:bg-slate-900 bg-(--ui-bg)"
-  >
-    <div class="container mx-auto flex justify-between items-center">
+  <header class="sticky top-0 left-0 w-full border-b dark:bg-slate-900 bg-(--ui-bg)">
+    <div class="container py-4 md:px-6 px-5 mx-auto flex justify-between items-center">
       <ULink :to="{ name: 'home' }" class="text-2xl font-bold">Todo App</ULink>
       <AuthBoutons />
     </div>
   </header>
 </template>
-
-<script setup lang="ts"></script>
 
 <style scoped></style>
