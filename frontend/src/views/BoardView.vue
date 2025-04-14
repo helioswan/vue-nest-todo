c08042d61126f0019d52e7bf426218be50cf4ae6
helioswan <helios.wan@hotmail.fr>
2025-04-14 15:28:27 +0200

    Fix mobile board view

--- frontend/src/views/BoardView.vue
+++ frontend/src/views/BoardView.vue
@@ -27,14 +27,18 @@ const tasks = reactive({
   Done: [] as Task[],
 })
 
-onMounted(() => {
-  boardStore.fetchBoard(route.params.id as string)
-  taskStore.fetchTasks()
+function filterTasks() {
   if (taskStore.tasks) {
     tasks['To do'] = filterTaskByStatus(taskStore.tasks, TaskStatus.TODO)
     tasks['In progress'] = filterTaskByStatus(taskStore.tasks, TaskStatus.IN_PROGRESS)
     tasks['Done'] = filterTaskByStatus(taskStore.tasks, TaskStatus.DONE)
   }
+}
+
+onMounted(() => {
+  boardStore.fetchBoard(route.params.id as string)
+  taskStore.fetchTasks()
+  filterTasks()
 })
 </script>
 
@@ -52,9 +56,9 @@ onMounted(() => {
         <h1 class="text-2xl font-bold">{{ boardStore.board?.title }}</h1>
       </div>
       <ul class="flex gap-8 overflow-x-auto">
-        <li v-for="(list, listIndex) in lists" class="w-96" :key="listIndex">
+        <li v-for="(list, listIndex) in lists" class="md:min-w-96 min-w-72" :key="listIndex">
           <TaskList :title="list.title" :color="list.color">
-            <li v-for="(task, taskIndex) in tasks[list.title]" :key="taskIndex">
+            <li v-for="(task, taskIndex) in tasks[list.status]" :key="taskIndex">
               <TaskCard :task />
             </li>
             <li v-if="list.status === TaskStatus.TODO">
