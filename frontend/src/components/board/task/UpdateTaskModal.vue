2996fa242b5164b7cb551ab14cc196b6690ca397
helioswan <helios.wan@hotmail.fr>
2025-04-14 13:57:41 +0200

    Add task actions modal

---
+++ frontend/src/components/board/task/UpdateTaskModal.vue
@@ -0,0 +1,100 @@
+<script setup lang="ts">
+import * as z from 'zod'
+import { reactive, type Ref } from 'vue'
+import { useTaskStore } from '@/stores/task.store'
+import { TaskStatus } from '@/enums/task-status.enum'
+
+const { updateTask } = useTaskStore()
+
+const open: Ref<boolean | undefined> = defineModel()
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+})
+
+const schema = z.object({
+  title: z.string({
+    required_error: 'Title is required',
+  }),
+  description: z.string({
+    required_error: 'Description is required',
+  }),
+  status: z.nativeEnum(TaskStatus),
+})
+
+type Schema = z.output<typeof schema>
+
+const state = reactive<Partial<Schema>>({
+  title: props.title,
+  description: props.title,
+  status: props.status as TaskStatus,
+})
+
+function closeModal() {
+  open.value = false
+}
+
+async function onSubmit() {
+  const result = schema.safeParse(state)
+
+  if (!result.success) {
+    return console.log(result.error.formErrors.fieldErrors)
+  }
+
+  const { title, description, status } = result.data
+  await updateTask(
+    {
+      title,
+      description,
+      status: status,
+    },
+    props.id,
+  )
+  closeModal()
+}
+</script>
+
+<template>
+  <UModal v-model:open="open" title="Edit task" :ui="{ footer: 'justify-end' }">
+    <template #body>
+      <UForm :schema="schema" :state="state" class="space-y-6">
+        <UFormField label="Title" name="title" required>
+          <UInput v-model="state.title" placeholder="Rename your task" class="block" />
+        </UFormField>
+        <UFormField label="Description" name="description" required>
+          <UTextarea
+            v-model="state.description"
+            placeholder="Type your description..."
+            class="block"
+          />
+        </UFormField>
+        <UFormField label="Task status" name="status" required>
+          <USelectMenu
+            v-model="state.status"
+            :items="['To do', 'In progress', 'Done']"
+            class="w-48"
+          />
+        </UFormField>
+      </UForm>
+    </template>
+
+    <template #footer>
+      <UButton label="Edit" color="neutral" @click.prevent="onSubmit" />
+      <UButton label="Cancel" color="neutral" variant="outline" @click.prevent="closeModal" />
+    </template>
+  </UModal>
+</template>
