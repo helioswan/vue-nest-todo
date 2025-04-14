e0a1f0b2f3f23a5b034cc4404dde5db63f96ac22
helioswan <helios.wan@hotmail.fr>
2025-04-14 13:53:06 +0200

    Update boardcard  style

--- frontend/src/components/board/BoardCard.vue
+++ frontend/src/components/board/BoardCard.vue
@@ -1,7 +1,20 @@
+<script setup lang="ts">
+import type { Board } from '@/interfaces/board/board.entity'
+import { formatDate } from '../../utils/date-formatter.util'
+import RenameBoardModal from '@/components/board/RenameBoardModal.vue'
+
+const props = defineProps({
+  board: { type: Object as () => Board, required: true },
+})
+</script>
+
 <template>
   <UCard>
     <template #header>
-      <h3 class="text-xl font-bold">{{ props.board.title }}</h3>
+      <div class="flex justify-between flex-wrap">
+        <h3 class="text-xl font-bold">{{ props.board.title }}</h3>
+        <RenameBoardModal :id="props.board._id" :title="props.board.title" />
+      </div>
     </template>
     <p>Created at: {{ formatDate(props.board.createdAt) }}</p>
     <p>Updated at: {{ formatDate(props.board.updatedAt) }}</p>
@@ -10,19 +23,8 @@
         <UButton icon="i-lucide-eye" :to="{ name: 'board', params: { id: props.board._id } }"
           >View</UButton
         >
-        <RenameBoardModal :id="props.board._id" :title="props.board.title" />
         <DeleteBoardModal :id="props.board._id" :title="props.board.title" />
       </div>
     </template>
   </UCard>
 </template>
-
-<script setup lang="ts">
-import type { Board } from '@/interfaces/board/board.entity'
-import { formatDate } from '../../utils/date-formatter'
-import RenameBoardModal from '@/components/board/RenameBoardModal.vue'
-
-const props = defineProps({
-  board: { type: Object as () => Board, required: true },
-})
-</script>
